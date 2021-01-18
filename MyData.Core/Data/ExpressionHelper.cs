3bb696fb072472275df35205fe0b1db8ddc8ea3c
beyt34 <beyt34@gmail.com>
2021-01-18 15:02:53 +0300

    .

--- MyData.Core/Data/ExpressionHelper.cs
+++ MyData.Core/Data/ExpressionHelper.cs
@@ -2,7 +2,7 @@
 using System.Linq.Expressions;
 
 namespace MyData.Core.Data {
-    public static class LinqExpressionHelper {
+    public static class ExpressionHelper {
         public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2) {
             var parameter = Expression.Parameter(typeof(T));
 
